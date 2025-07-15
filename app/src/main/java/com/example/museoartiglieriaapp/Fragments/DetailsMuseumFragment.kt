83cc39e5d223cc53be3c2337223f9815c6b35108
ovetto2025 <luca.carlino@edu-its.it>
2025-07-16 01:29:55 +0200

    fixati problemi con il carosello e tts

--- app/src/main/java/com/example/museoartiglieriaapp/Fragments/DetailsMuseumFragment.kt
+++ app/src/main/java/com/example/museoartiglieriaapp/Fragments/DetailsMuseumFragment.kt
@@ -13,16 +13,7 @@ import android.widget.TextView
 import android.widget.ImageView
 import java.util.Locale
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [DetailsMuseumFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DetailsMuseumFragment : Fragment() {
 
 
