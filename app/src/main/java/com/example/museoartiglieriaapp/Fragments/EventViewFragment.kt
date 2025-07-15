21e52360c5e6bd000bf5a59ca1e9ec7cfcc38761
ovetto2025 <luca.carlino@edu-its.it>
2025-07-16 01:20:13 +0200

    fixati problemi con il carosello e tts

--- app/src/main/java/com/example/museoartiglieriaapp/Fragments/EventViewFragment.kt
+++ app/src/main/java/com/example/museoartiglieriaapp/Fragments/EventViewFragment.kt
@@ -6,10 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
+import android.widget.TextView
+import android.speech.tts.TextToSpeech
 import com.example.museoartiglieriaapp.R
-
+import java.util.Locale
 
 class EventViewFragment : Fragment() {
+    private var tts: TextToSpeech? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,5 +32,36 @@ class EventViewFragment : Fragment() {
                 .replace(R.id.fragment_container, HomeFragment())
                 .commit()
         }
+
+        // Inizializzazione delle view per il TTS
+        val ttsButton = view.findViewById<View>(R.id.ttsButton)
+        val title = view.findViewById<TextView>(R.id.event_title)
+
+        val description = view.findViewById<TextView>(R.id.event_description_part1)
+        val description2 = view.findViewById<TextView>(R.id.event_description_part2)
+
+        // Inizializzazione del TTS
+        tts = TextToSpeech(requireContext()) { status ->
+            if (status == TextToSpeech.SUCCESS) {
+                tts?.language = Locale.ITALIAN
+            }
+        }
+
+        // Gestione del click sul bottone TTS
+        ttsButton?.setOnClickListener {
+            val textToRead = StringBuilder().apply {
+                append("${title?.text}. ")
+                append("${description?.text}. ")
+                append("${description2?.text}")
+            }.toString()
+
+            tts?.speak(textToRead, TextToSpeech.QUEUE_FLUSH, null, null)
+        }
+    }
+
+    override fun onDestroyView() {
+        tts?.stop()
+        tts?.shutdown()
+        super.onDestroyView()
     }
 }
